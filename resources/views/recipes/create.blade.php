31035343fa667b6b2671103030eae831a547cbcf
Krystian Cichorz <krystiancichorz708@gmail.com>
2023-12-26 01:57:37 +0100

    Create segment

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -3,38 +3,67 @@
 @section('content')
 <div class="w-full h-screen flex items-center justify-center">
     <div
-        class="w-full md:w-2/3 lg:w-1/3 h-3/7 rounded-xl bg-oldrose m-4 md:m-0 p-8 md:shadow-2xl shadow-oldrose flex flex-col justify-center">
+        class="w-full md:w-2/3 lg:w-2/3 xl:w-3/5 h-3/7 rounded-xl bg-oldrose m-4 md:m-0 p-8 md:shadow-2xl shadow-oldrose flex flex-col justify-center">
         <h2 class="w-full text-3xl font-bold tracking-wide text-center pb-4">{{__('Create Recipe')}}</h2>
-        <form method="POST" action="{{ route('recipe.store') }}" class="flex flex-col">
+        <form method="POST" action="{{ route('recipe.store') }}" class="flex flex-col md:grid md:grid-cols-2 gap-x-8">
             @csrf
-            <!-- Name -->
-            <label for="name" class="text-xl relative mb-6">{{__('Name')}}
-                <input id="name" type="text" class="w-full rounded-xl p-2 @error('name')
-                 border-vanilla @enderror" name="name" value="{{ old('name') }}" required autocomplete="name"
+            <label for="title"
+                class="text-xl font-bold relative gap-6 mb-6 grid grid-cols-2 col-span-2 text-center h-full justify-center items-center md:border-b-2 border-vanilla">{{__('Title')}}
+                <input id="title" type="text" class="w-full rounded-xl p-2 @error('title')
+                 border-vanilla @enderror" name="title" value="{{ old('title') }}" required autocomplete="title"
                     autofocus>
-                @error('name')
+                @error('title')
                 <span class="absolute top-full left-0 text-vanilla text-sm md:text-base" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
                 @enderror
             </label>
-            <!-- Description -->
-            <label for="description" class="text-xl relative mb-6">{{__('Description')}}
-                <textarea id="description" type="text" class="w-full rounded-xl p-2 @error('description')
-                border-vanilla @enderror" name="description" value="{{ old('description') }}" required
-                    style="max-height: 15em; resize: vertical;"></textarea>
-                @error('description')
+
+            <label for="prep_time"
+                class="text-xl font-bold relative gap-6 mb-6 grid grid-cols-2 text-center h-full justify-center items-center">{{__('Prep
+                Time')}}
+                <input id="prep_time" type="time" class="rounded-xl p-2 @error('prep_time')
+                 border-vanilla @enderror" name="prep_time" value="{{ old('prep_time') }}" required
+                    autocomplete="prep_time" autofocus>
+                @error('prep_time')
                 <span class="absolute top-full left-0 text-vanilla text-sm md:text-base" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
                 @enderror
             </label>
-            <label for="category" class="text-xl relative mb-6">{{__('Category')}}
-                <select id="category" type="text" class="w-full rounded-xl p-2 @error('category')
-                 border-vanilla @enderror" name="category" value="{{ old('category') }}" required
-                    autocomplete="category" autofocus>
-                    @foreach($categorySanitized as $id => $name)
-                    <option value="{{$id}}">{{$name}}</option>
+
+            <label for="cook_time"
+                class="text-xl font-bold relative gap-6 mb-6 grid grid-cols-2 text-center h-full justify-center items-center">{{__('Cook
+                Time')}}
+                <input id="cook_time" type="time" class="rounded-xl p-2 @error('cook_time')
+                 border-vanilla @enderror" name="cook_time" value="{{ old('cook_time') }}" required
+                    autocomplete="cook_time" autofocus>
+                @error('cook_time')
+                <span class="absolute top-full left-0 text-vanilla text-sm md:text-base" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </label>
+
+            <label for="servings"
+                class="text-xl font-bold relative gap-6 mb-6 grid grid-cols-2 text-center h-full justify-center items-center">{{__('Servings')}}
+                <input id="servings" type="number" min="1" max="30" step="1" class="rounded-xl p-2 @error('servings')
+                 border-vanilla @enderror" name="servings" value="{{ old('servings') }}" required
+                    autocomplete="servings" autofocus>
+                @error('servings')
+                <span class="absolute top-full left-0 text-vanilla text-sm md:text-base" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </label>
+
+            <label for="category"
+                class="text-xl font-bold relative gap-6 mb-6 grid grid-cols-2 text-center h-full justify-center items-center">{{__('Category')}}
+                <select id="category" class="w-full rounded-xl p-2 @error('category')
+                border-vanilla @enderror" name="category" required>
+                    <option value="" disabled selected>{{__('Select Category')}}</option>
+                    @foreach ($categorySanitized as $id => $name)
+                    <option value="{{ $id }}">{{ $name }}</option>
                     @endforeach
                 </select>
                 @error('category')
@@ -43,6 +72,39 @@
                 </span>
                 @enderror
             </label>
+
+            <label for="ingredients"
+                class="text-xl font-bold relative gap-6 mb-6 grid grid-cols-2 col-span-2 text-center h-full justify-center items-center md:border-t-2 border-vanilla">{{__('Ingredients')}}
+                <textarea id="ingredients" class="w-full rounded-xl p-2 @error('ingredients')
+                border-vanilla @enderror" name="ingredients" required
+                    style="max-height: 6em; resize: vertical;">{{ old('ingredients') }}</textarea>
+                @error('ingredients')
+                <span class="absolute top-full left-0 text-vanilla text-sm md:text-base" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </label>
+
+            <label for="instructions"
+                class="text-xl font-bold relative gap-6 mb-6 grid grid-cols-2 col-span-2 text-center h-full justify-center items-center">{{__('Instructions')}}
+                <textarea id="instructions" class="w-full rounded-xl p-2 @error('instructions')
+                border-vanilla @enderror" name="instructions" required
+                    style="max-height: 6em; resize: vertical;">{{ old('instructions') }}</textarea>
+                @error('instructions')
+                <span class="absolute top-full left-0 text-vanilla text-sm md:text-base" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </label>
+
+            <div class="col-span-1 mb-4 md:mb-0 flex w-full">
+                <a href="{{ route('home')}}"
+                    class="bg-vanilla text-white text-center rounded-xl px-4 py-2 w-full h-full">{{__('Back')}}</a>
+            </div>
+            <div class="col-span-1">
+                <button type="submit"
+                    class="bg-vanilla text-white rounded-xl px-4 py-2 w-full h-full">{{__('Create')}}</button>
+            </div>
         </form>
     </div>
 </div>
